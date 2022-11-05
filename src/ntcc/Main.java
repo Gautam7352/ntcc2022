53f680e606d203080f9b02f86985fa86b8b6209d
Gautam Kumar <Gautam7352@gmail.com>
2022-11-05 16:22:48 +0530

    Initial commit

---
+++ src/ntcc/Main.java
@@ -0,0 +1,18 @@
+package ntcc;
+
+public class Main {
+    public static void main(String[] args) {
+        Tasklist array is declared
+        Queue array is declared
+        declaration of queue variable
+        Initializing object in Tasklist array
+        Burst time filling
+        Arrival time filling
+        Deadline time filling
+        Membership type filling
+        
+        Timer starts
+                
+
+    }
+}
---
+++ src/ntcc/Tasks.java
@@ -0,0 +1,13 @@
+package ntcc;
+
+public class Tasks {
+
+    int burst_time=0;
+
+    int arrival_time=0;
+    int deadline=0;
+
+    int waiting_time=0;
+    float membership_type=0.0f;
+
+}
